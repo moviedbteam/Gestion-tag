2813f0049aaabc7720bff5b0dd92602824346d38
DESKTOP-F1TL2CH\jchay <joel.chaysinh@epita.fr>
2023-02-22 16:31:42 +0100

    Ajout de Spring Security

--- exposition/src/main/java/com/bcefit/projet/Starter.java
+++ exposition/src/main/java/com/bcefit/projet/Starter.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.bcefit.projet"}, lazyInit = true)
+@ComponentScan(basePackages = {"com.bcefit.*"}, lazyInit = true)
 public class Starter {
     public static void main(String[] args) {
         SpringApplication.run(Starter.class, args);}
